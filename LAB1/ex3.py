60ffede9e91e3c70da59f2537916bedba616cdd7
Ivan_Der <tumaskinivan@gmail.com>
2024-01-20 18:26:42 +0600

    Finish ex3

--- LAB1/ex3.py
+++ LAB1/ex3.py
@@ -1,1 +1,12 @@
-print("Hello World")
+carname="Volvo"
+x=50
+x=5
+y=10
+print(x+y)
+z=x+y
+print(z)
+x,y,z= "Orange", "Banana", "Cherry"
+x=y=z= "Orange"
+def myfunc():
+  global x
+  x = "fantastic"
