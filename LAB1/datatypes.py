aae4ea7729d4240c90aac405eb6ce24e276f570b
Ivan_Der <tumaskinivan@gmail.com>
2024-01-21 14:56:58 +0600

    Finish 4 item

---
+++ LAB1/datatypes.py
@@ -0,0 +1,16 @@
+x=5
+print(type(x))
+x = "Hello World"
+print(type(x))
+x = 20.5
+print(type(x))
+x = ["apple", "banana", "cherry"]
+print(type(x))
+x = ("apple", "banana", "cherry")
+print(type(x))
+x = {"name" : "John", "age" : 36}
+print(type(x))
+x=True
+print(type(x))
+
+
