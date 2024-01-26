21266bd65b1d3e752e0e82e117e69d26079f0c40
Ivan_Der <tumaskinivan@gmail.com>
2024-01-26 23:03:40 +0600

    Finish 3 item

---
+++ LAB2/operators.py
@@ -0,0 +1,12 @@
+print(10*5) #if we need to mupliple , use "*"
+print(10/5) #if we need to mupliple , use "/"
+
+fruits = ["apple", "banana"]
+if "apple" in fruits:
+    print("Yes, apple is a fruit!")  #use "in" to check whether the input object belongs to the given one
+
+if 5 != 10:
+  print("5 and 10 is not equal") #use "!=" to check that "5" not equal "10"
+
+if 5 == 10 or 4 == 4:
+  print("At least one of the statements is true") #use "or" to check that at least one statement are "True"
