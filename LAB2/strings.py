b0fad889f199d0db1aef9668247b98d2b274428a
Ivan_Der <tumaskinivan@gmail.com>
2024-01-26 22:37:26 +0600

    Finish 1 item

--- LAB2/strings.py
+++ LAB2/strings.py
@@ -1,2 +1,20 @@
 x = "Hello World"
-print(len(x))
+print(len(x)) #lenght of input word
+txt = "Hello World"
+x = txt[0]   #first symbol(first character of the string)
+txt = "Hello World"
+x = txt[2:5] #characters from index 2 to index 4 (5 because we need to include 4-th)
+txt = "Hello World"
+x = txt.strip() #return the string without any whitespace at the beginning or the end.
+txt = "Hello World"
+txt = txt.upper() #convert the value of input "txt" to upper case.
+txt = "Hello World"
+txt = txt.lower() #convert the value of input "txt" to lower case.
+txt = "Hello World"
+txt = txt.replace("H","J") #replace the character H with a J (they can be any from a to Z)
+age = 36
+txt = "My name is John, and I am {}"
+print(txt.format(age))  #we use "{}" to insert the correct syntax and add a placeholder for the "age" parameter.
+
+
+
