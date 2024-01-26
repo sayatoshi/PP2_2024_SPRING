fe017b7fe6e11f31c39e80ca046f805b69ba4b93
Ivan_Der <tumaskinivan@gmail.com>
2024-01-26 22:53:28 +0600

    Finish 2 item

---
+++ LAB2/booleans.py
@@ -0,0 +1,5 @@
+print(10>9)  #for Boolean answer is True
+print(10==9)  #for Boolean answer is False
+print(10<9)  #for Boolean answer is False
+print(bool("abc")) #for Boolean answer is True
+print(bool(0)) #for Boolean answer is False
