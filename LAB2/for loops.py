d55a60c59d4c27cb62019f4fc5e31747e3e1d24e
Ivan_Der <tumaskinivan@gmail.com>
2024-01-27 19:04:03 +0600

    Finish 10 item

---
+++ LAB2/for loops.py
@@ -0,0 +1,19 @@
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    print(x)
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    continue
+print(x)
+
+
+for x in range(1,6):
+  print(x)
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    break
+print(x)
