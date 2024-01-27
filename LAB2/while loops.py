04bef6bf727d33e38bd79e752211eeaedf9be584
Ivan_Der <tumaskinivan@gmail.com>
2024-01-27 18:58:49 +0600

    Finish 9 item

---
+++ LAB2/while loops.py
@@ -0,0 +1,26 @@
+i = 1
+while i < 6:
+    print(i)
+    i += 1
+
+j = 1
+while j < 6:
+    if j == 3:
+        break
+    j += 1
+    print(j)
+
+k = 0
+while k < 6:
+    k += 1
+    if k == 3:
+        continue
+    print(k)
+
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+else:
+
+  print("i is no longer less than 6")
