f3af00c82ccfca0246d0fd2dc9530b8c87bf24b2
Ivan_Der <tumaskinivan@gmail.com>
2024-02-05 16:57:29 +0600

    Finish 5 item

---
+++ LAB3/functions2#5.py
@@ -0,0 +1,95 @@
+movies = [
+{
+"name": "Usual Suspects", 
+"imdb": 7.0,
+"category": "Thriller"
+},
+{
+"name": "Hitman",
+"imdb": 6.3,
+"category": "Action"
+},
+{
+"name": "Dark Knight",
+"imdb": 9.0,
+"category": "Adventure"
+},
+{
+"name": "The Help",
+"imdb": 8.0,
+"category": "Drama"
+},
+{
+"name": "The Choice",
+"imdb": 6.2,
+"category": "Romance"
+},
+{
+"name": "Colonia",
+"imdb": 7.4,
+"category": "Romance"
+},
+{
+"name": "Love",
+"imdb": 6.0,
+"category": "Romance"
+},
+{
+"name": "Bride Wars",
+"imdb": 5.4,
+"category": "Romance"
+},
+{
+"name": "AlphaJet",
+"imdb": 3.2,
+"category": "War"
+},
+{
+"name": "Ringing Crime",
+"imdb": 4.0,
+"category": "Crime"
+},
+{
+"name": "Joking muck",
+"imdb": 7.2,
+"category": "Comedy"
+},
+{
+"name": "What is the name",
+"imdb": 9.2,
+"category": "Suspense"
+},
+{
+"name": "Detective",
+"imdb": 7.0,
+"category": "Suspense"
+},
+{
+"name": "Exam",
+"imdb": 4.2,
+"category": "Thriller"
+},
+{
+"name": "We Two",
+"imdb": 7.2,
+"category": "Romance"
+}
+]
+
+def func(movies_list, category):
+    category_movies = [movie for movie in movies_list if movie["category"] == category]
+
+    
+
+    total_score = sum(movie["imdb"] for movie in category_movies)
+    average_score = total_score / len(category_movies)
+
+    return average_score
+
+category_input = input("Enter a category: ")
+average_score_in_category = func(movies, category_input)
+print(f"The average IMDB score in the category '{category_input}' is: {average_score_in_category:.2f}")
+
+
+
+
