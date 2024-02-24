80559f43c7ab520a9bf76b909a58ce30a2d7c130
Ivan_Der <tumaskinivan@gmail.com>
2024-02-24 23:50:50 +0600

    Finish 8 item

---
+++ LAB5/regex/regex5-8.py
@@ -0,0 +1,10 @@
+import re
+
+def func(text):
+    pattern = r"[A-Z]+"
+    replaced_text = re.sub(pattern, ':', text)
+    return replaced_text
+
+input_text = input("Enter a string: ")
+result = func(input_text)
+print("Result:", result)
